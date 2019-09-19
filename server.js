826c8a0646f6dc4214cf041026a2aebba4740db8
Jordan Mullen <jordan.mullen@hotmail.com>
2019-09-19 11:31:18 -0600

    adding new relic addon to see where the request is stopping

--- server.js
+++ server.js
@@ -7,15 +7,21 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const routes = require('./routes')
 const path = require('path')
+const timeout = require('connect-timeout')
 
 const port = process.env.PORT || 8000
 
 const app = express()
 
+app.use(timeout('30s'))
 app.use(bodyParser.json())
+app.use(haltOnTimedout)
 app.use(express.urlencoded({ extended: true}))
+app.use(haltOnTimedout)
 app.use(cors())
+app.use(haltOnTimedout)
 app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(haltOnTimedout)
 
 
 // Defining the routes to use
@@ -25,6 +31,10 @@ app.use('/api', routes.data)
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
+
+function haltOnTimedout (req, res, next) {
+    if (!req.timedout) next()
+}
 
 app.listen(port, () => 
     console.log(`App listening on port ${port}`)
